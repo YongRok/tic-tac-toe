c6b0eca0194885b97d01fa4e2bb83df73fd536e2
Yongrok Kim <kimyr222@naver.com>
2017-11-26 16:05:34 +0900

    tic

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -12,9 +12,11 @@ def checked(i) :
       if player == "X" :
             player = "O"
             button["bg"] = "yellow"
+            checkedWinner()
       else :
             player = "X"
             button["bg"] = "lightgreen"
+            checkedWinner()
 
 window = Tk()
 player = "X"
@@ -25,6 +27,31 @@ for i in range(9) :
       b.grid(row=i//3, column=i%3)
       list.append(b)
 
+def printWinner():
+      if player=="O":
+            print("X win !")
+            quit()
+      elif player=="X":
+            print("0 win !")
+            quit()
+            
+def checkedWinner():
+      if list[0]["text"]==list[1]["text"]==list[2]["text"] != "     ":
+            printWinner()
+      elif list[3]["text"]==list[4]["text"]==list[5]["text"] != "     ":
+            printWinner()
+      elif list[6]["text"]==list[7]["text"]==list[8]["text"] != "     ":
+            printWinner()
+      elif list[0]["text"]==list[3]["text"]==list[6]["text"] != "     ":
+            printWinner()
+      elif list[1]["text"]==list[4]["text"]==list[7]["text"] != "     ":
+            printWinner()
+      elif list[2]["text"]==list[5]["text"]==list[8]["text"] != "     ":
+            printWinner()
+      elif list[0]["text"]==list[4]["text"]==list[8]["text"] != "     ":
+            printWinner()
+      elif list[2]["text"]==list[4]["text"]==list[6]["text"] != "     ":
+            printWinner()
 window.mainloop()
 
 
